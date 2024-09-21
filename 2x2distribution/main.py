a379c522d5c5af9deda681736c8decd955f09b20
Alejandro Soriano <alejandro.scompta@alumnos.upm.es>
2024-09-21 11:30:40 +0200

    Limpieza

--- 2x2distribution/main.py
+++ 2x2distribution/main.py
@@ -3,6 +3,6 @@ import time
 
 inicio = time.time()
 
-#calculos.distribucion()
-calculos.generar_combinaciones()
+calculos.distribucion()
+#calculos.generar_combinaciones()
 print("Resuelto en ", round(time.time() - inicio, 2))
