d4f1e9b4a9bd87e6e351f42c5f7682e8e1c89a1d
Alejandro Soriano <alejandro.scompta@alumnos.upm.es>
2025-02-16 19:15:08 +0100

    Added nxnxn in Haskell

---
+++ NumStates_nxnxn/states_nxnxn.hs
@@ -0,0 +1,45 @@
+--This program calculates the number of different states of a nxnxn Rubik's Cube
+
+--main :: IO ()
+--main = do
+--    putStrLn "Introduce un número:"
+--    input <- getLine
+--    let num = read input :: Int
+--    putStrLn $ "Has introducido: " ++ show num
+--
+
+main :: IO()
+main = do
+    --putStrLn "Insert the number of n"
+    --input <- getLine
+    --let n = read input :: Integer
+    --putStrLn $ "Number of states is " ++ (show $ states n)
+    --putStrLn $ "This number has " ++ (show $ numDigits $ states n) ++ " digits"
+    --return ()
+    putStrLn $ show $ map (states) [2..7]
+
+
+
+states :: Integer -> Integer
+states n
+    | n == 1 = 1
+    | n <= 0 = 0
+    | parity == 0 = div (((!)7) * (3 ^ 6) * (((!)24) ^ (k * (k-1))) ) (((!)4) ^ (6* ((k-1)^2)) )
+    | parity == 1 = div (((!)8) * (3 ^ 7) * ((!)12) * (2^10)* (((!)24) ^ ((k+1) * (k-1))) ) (((!)4) ^ (6* (k*(k-1))) )
+    | otherwise = 0
+
+    where
+        (k, parity) = n `quotRem` 2
+
+--We express n = 2*k + parity
+
+(!) :: Integer -> Integer
+(!) n 
+    | n <= 1 = 1
+    | otherwise = product [2 .. n]
+
+numDigits :: Integer -> Integer
+numDigits n
+    | n == 0 = 1
+    | otherwise = floor(1 + logBase 10 (fromInteger $ abs n))
+
